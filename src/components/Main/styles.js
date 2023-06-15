a4dd9645ff7ea960a5b7b7c2a0abdf79854e98b2
Yevgen <evgennevidim@gmail.com>
2023-06-15 23:29:30 +0300

    first commit

---
+++ src/components/Main/styles.js
@@ -0,0 +1,222 @@
+import bg from '../../../public/assets/bg_1.png'
+
+const styles = {
+    container: {
+        width: "100%",
+        maxWidth: "1350px",
+        height: "100%",
+        maxHeight: "750px"
+    },
+    home: {
+        position: "relative",
+        boxSizing: "border-box",
+        display: "flex",
+        background: `url("assets/bg_1.png")`,
+        backgroundSize: "cover",
+        top: 0,
+        left: 0,
+        width: "96%",
+        height: "100%",
+        maxHeight: "655px",
+    },
+    box__auth: {
+        display: "flex",
+        width: "60px",
+        height: "70px",
+        margin: "10px 40px"
+    },
+    btn__auth: {
+        display: "block",
+        border: "1px solid #FFFFFF",
+        backgroundColor: "#000000",
+        width: "90px",
+        height: "40px",
+        margin: "0 40px",
+        color: "#000000",
+        textDecoration: "none",
+    },
+    menu__but__1: {
+        padding: "10px 15px",
+        position: "relative",
+        background: "#000000",
+        color: "#ffffff",
+    },
+    auth__text: {
+        color: "#000000",
+        margin: "10px 0"
+    },
+    btn__auth__1: {
+        display: "block",
+        border: "1px solid #FFFFFF",
+        width: "90px",
+        height: "40px",
+        margin: "10px 50px",
+        color: "#FFFFFF"
+    },
+    right__box: {},
+    mouse: {
+        display: "block",
+        width: "30px",
+        height: "30px",
+    },
+    img: {
+        width: "100%",
+        maxWidth: "120px",
+        margin: "20px 0 0 20px",
+    },
+    menu__but: {
+        padding: "10px 15px",
+        position: "relative",
+        background: "#000000",
+        color: "#ffffff",
+    },
+    text: {
+        fontWeight: 500,
+        fontSize: "56px",
+        lineHeight: "72px",
+        textAlign: "center",
+        color: "#ffffff",
+        position: "relative",
+    },
+    box_img: {
+        width: "100%",
+        maxWidth: "140px",
+        height: "50px",
+        position: "relative",
+        margin: "20px 30px"
+    },
+    box_typo: {
+        display: "flex",
+        width: "750px",
+        maxWidth: "750px",
+        position: "relative",
+        height: "450px",
+        margin: "190px auto"
+    },
+    btn: {
+        padding: "10px 23px",
+        position: "relative",
+        background: "#40C7F4",
+        color: "#ffffff"
+    },
+    footer__home: {
+        position: "absolute",
+        bottom: "-40px",
+        margin: "50px 150px",
+        width: "1100px",
+        height: "250px",
+    },
+    footer__home__1: {
+        position: "relative",
+        display: "inline-block",
+        width: "100%",
+        maxWidth: "290px",
+        height: "100%",
+        maxHeight: "200px",
+    },
+    footer__home__2: {
+        position: "relative",
+        display: "inline-block",
+        width: "270px",
+        height: "250px",
+    },
+    footer__home__3: {
+        position: "relative",
+        display: "inline-block",
+        width: "270px",
+        height: "250px",
+    },
+    footer__home__4: {
+        position: "relative",
+        display: "inline-block",
+        width: "270px",
+        height: "250px",
+    },
+    burger: {
+        width: "15px",
+        height: "30px",
+        display: "block",
+        margin: "0 -15px",
+        cursor: "pointer",
+    },
+    social: {
+        width: "70px",
+        height: "80px",
+        display: "flex",
+        flexDirection: "column",
+    },
+    facebook: {
+        textAlign: "center",
+        display: "block",
+        cursor: "pointer",
+        width: "35px",
+        height: "40px",
+        marginBottom: "10px"
+    },
+    twitter: {
+        textAlign: "center",
+        cursor: "pointer",
+        display: "block",
+        width: "35px",
+        height: "40px",
+        marginBottom: "10px"
+    },
+    insta: {
+        textAlign: "center",
+        cursor: "pointer",
+        display: "block",
+        width: "35px",
+        height: "40px",
+        marginBottom: "10px"
+    },
+    box__social: {
+        height: "550px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        position: "absolute",
+        right: "-80px",
+        top: "25px",
+    },
+    text__bio: {
+        color: "#ffffff",
+        fontWeight: "500",
+        paddingTop: "10px"
+    },
+    text__01: {
+        display: "block",
+        width: "70px",
+        height: "70px",
+    },
+    box__img: {
+        display: "block",
+        width: "80px",
+        height: "80px",
+    },
+    text__02: {
+        fontSize: "120px"
+    },
+    text__03: {
+        fontSize: "120px"
+    },
+    text__04: {
+        fontSize: "120px"
+    },
+    text__para__01: {
+        color: "#ffffff",
+        padding: "10px 0"
+    },
+    text__para__02: {
+        color: "#ffffff",
+        marginTop: "15px"
+    },
+    text__para__03: {
+        color: "#ffffff",
+        marginTop: "15px"
+    },
+    text__para__04: {
+        color: "#ffffff",
+        marginTop: "15px"
+    },
+}
+export default styles;
